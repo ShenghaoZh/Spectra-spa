dae4b8ac3693628b00db9b353bdb6ed4f88d157d
ShenghaoZh <shenghao@wustl.edu>
2019-10-22 09:50:15 -0400

    Updates

---
+++ precache-manifest.2323a31f1952f79bcb7e0a8f23e3a2e1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df37fb768313543bef182ce10b5830ec",
+    "url": "/Spectra-spa/index.html"
+  },
+  {
+    "revision": "73b5d4dac08b0fd52dc4",
+    "url": "/Spectra-spa/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "a21941762523cb7c891a",
+    "url": "/Spectra-spa/static/css/main.e7db3495.chunk.css"
+  },
+  {
+    "revision": "73b5d4dac08b0fd52dc4",
+    "url": "/Spectra-spa/static/js/2.6eb7e2eb.chunk.js"
+  },
+  {
+    "revision": "a21941762523cb7c891a",
+    "url": "/Spectra-spa/static/js/main.ddf4d85b.chunk.js"
+  },
+  {
+    "revision": "13b1b09a2165f0751839",
+    "url": "/Spectra-spa/static/js/runtime~main.ce5957ce.js"
+  },
+  {
+    "revision": "9ee61ab9b4227091fff07d5d8da669d7",
+    "url": "/Spectra-spa/static/media/avatar-z.9ee61ab9.png"
+  },
+  {
+    "revision": "f355c337b6313b09ddb14b966edf7e74",
+    "url": "/Spectra-spa/static/media/uni.f355c337.jpg"
+  }
+]);
